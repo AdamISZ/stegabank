41006c5c03f54b75a1edd807feea082466fd9d97
AdamISZ <ekaggata@gmail.com>
2013-08-22 06:40:22 +0000

    added cleancap

---
+++ cleancap.py
@@ -0,0 +1,37 @@
+#Cleanup a capture file so there are no extraneous packets
+#Use a filter e.g. "ssl" to choose which packets to KEEP
+#(or set reverse = false in call to sharkutils.editcap if 
+#you want to remove them). The cleaned file is APPENDED with "_clean".
+#
+
+#=====LIBRARY IMPORTS===============
+import sys
+import subprocess
+import shutil
+import re
+import shared
+import sharkutils 
+import helper_startup
+#=====END LIBRARY IMPORTS==========
+
+if __name__ == "__main__":
+            
+    #Load all necessary configurations:
+    #========================
+    helper_startup.loadconfig()
+    
+    if len(sys.argv) < 2:
+        print 'Usage cleancap.py <capfile> <filter string>'
+        exit()
+    outfile = sys.argv[1] + "_clean"
+    sharkutils.editcap(sys.argv[1],outfile,filter=sys.argv[2], reverse = False)
+    
+    
+
+
+
+        
+
+        
+
+
